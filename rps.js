34dd0484e2d5dfa82c620c375dfb2f36f1f6afd2
SamyDz7 <samydz974@gmail.com>
2022-04-09 14:32:51 +0100

    Add play round

--- rps.js
+++ rps.js
@@ -12,12 +12,12 @@ declare winner
 
 
 */ 
-let choice;
+let computerplay;
 let userplay;
 function computerchoice() {
     let choices = ["rock", "paper", "scissors"];
-    choice = choices[Math.floor( Math.random() * choices.length)]
-    return choice;
+    computerplay = choices[Math.floor( Math.random() * choices.length)]
+    return computerplay;
 }
 computerchoice()
 
@@ -26,17 +26,50 @@ function checkplayerchoice() {
     while(userplay.toLowerCase()=="rock" != true && userplay.toLowerCase()=="paper" != true && userplay.toLowerCase()=="scissors" != true) {
         userplay = prompt("Choose Rock, Paper or Scissors:")
     }
-    return userplay ;
+    return userplay = userplay.toLowerCase() ;
 }
 checkplayerchoice()
-console.log(userplay)
         
-    
 
 checkplayerchoice()
 function playround(userplay , computerplay) {
+if (userplay == "rock") {
+    if (computerplay == "rock") {
+        console.log("It's a draw. ")
+    }
+    else if (computerplay =="paper") {
+        console.log("You lose! paper beats rock.")
+    }
+    else {
+        console.log("You win! rock beats scissors.")
+    }
+}
+if (userplay == "paper") {
+    if (computerplay == "paper") {
+        console.log("It's a draw. ")
+    }
+    else if (computerplay =="scissors") {
+        console.log("You lose! scissors beats paper.")
+    }
+    else {
+        console.log("You win! paper beats rock.")
+    }
+}
+
+if (userplay == "scissors") {
+    if (computerplay == "scissors") {
+        console.log("It's a draw. ")
+    }
+    else if (computerplay =="rock") {
+        console.log("You lose! rock beats scissors.")
+    }
+    else {
+        console.log("You win! scissors beats paper.")
+    }
+}
 
 
 
 
 }
+playround(userplay , computerplay)
